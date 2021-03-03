307c01ba80c469f28a90430fe4d1aa6aedddbf5d
unknown <edwardmaddenanson@gmail.com>
2021-03-02 19:16:10 -0600

    [Initial Commit]

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is the initial commit for the new blueville capital? application
         </p>
         <a
           className="App-link"
